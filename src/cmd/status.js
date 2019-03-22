12286605e359c99aa894f5e2b21216d87257368d
chrokh <christopher.okhravi@gmail.com>
2019-03-22 10:57:05 +0100

    Add 'lit expand suggestion' to status command and print more compactly.

--- src/cmd/status.js
+++ src/cmd/status.js
@@ -4,6 +4,7 @@ const { completedQueries, remainingQueries } = require('../vm/collect')
 
 if (!db.exists()) {
   console.log('No lit review in current directory.')
+  console.log('  (use "lit init" to initialize lit review)')
   return
 }
 
@@ -19,23 +20,33 @@ const documents    = all('document')
 const comQs        = completedQueries()
 
 
+
 // Print input info
 if (len(phrases) == 0) {
   console.log('No phrases or sets entered.')
   console.log('  (use "lit feed" to feed input data)')
   return
 }
-console.log(`${len(phrases)} phrases over ${len(sets)} sets,`)
-console.log(`  resulting in ${len(queries)} queries`)
+console.log(`${len(phrases)} phrases over ${len(sets)} sets entered.`)
 // TODO: Queries can become stale if you add more keywords.
 // Perhaps queries should always be generated on demand?
 
 
+
+// Print query info
+if (len(queries) == 0) {
+  console.log('No queries generated.')
+  console.log('  (use "lit expand" to expand phrase sets into queries)')
+  return
+}
+console.log(`${len(queries)} queries generated.`)
+
+
+
 // Print output info
-console.log()
 if (len(documents) == 0) {
   console.log('No documents collected.')
-  console.log('  (use "lit collect" to execute queries)')
+  console.log('  (use "lit collect" to collect documents)')
   return
 }
 console.log('Collection status:')

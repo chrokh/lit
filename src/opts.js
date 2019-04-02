cfe7e0d4ca64a5b45a492b65cae7373ae7b3d0d1
chrokh <christopher.okhravi@gmail.com>
2019-04-02 17:18:30 +0200

    Add module for parsing command line options.

---
+++ src/opts.js
@@ -0,0 +1,33 @@
+/*
+ * @sig String -> { k: String|Bool }
+ * @example
+ *   toSetting('--foo')     // { foo: true  }
+ *   toSetting('--foo=bar') // { foo: 'bar' }
+ */
+const toSetting = str => {
+  if (str.indexOf('--')) throw `Invalid argument ${str}`
+
+  const eqPos = str.indexOf('=')
+  const isBool = eqPos == -1
+
+  const key =
+    isBool ?
+    str.slice(2) :
+    str.slice(2, eqPos)
+
+  const value =
+    isBool ?
+    true :
+    str.slice(eqPos + 1)
+
+  return { [key]: value }
+}
+
+const toObj = args =>
+  args.
+    map(toSetting).
+    reduce((acc, obj) => Object.assign(acc, obj), {})
+
+module.exports = {
+  toObj
+}

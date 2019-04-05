4c3c7d758607a00f46e50c791f8a2042f553d804
chrokh <christopher.okhravi@gmail.com>
2019-04-05 14:51:01 +0200

    Color document id when printing list.

--- src/cmd/list.js
+++ src/cmd/list.js
@@ -1,10 +1,10 @@
+const chalk = require('chalk')
 const { pipe, map, join, filter, k, not } = require('../base')
 const { all } = require('../entity')
 const vmDocs = require('../vm/documents')
 const opts = require('../opts')
 
 // Parse options
-const ARGS = [...process.argv.slice(3)]
 const OPTS = opts.toObj({ format: 'simple' })([...process.argv.slice(3)])
 
 // Read data
@@ -17,8 +17,9 @@ const expandDocument = doc => {
   const { excerpt, author } = matches[0] // TODO: consolidate
   return { ...doc, excerpt, author }
 }
+const title = doc => doc.id.substring(0, 7)
 const toDocumentString = doc =>
-  `* ${doc.id.substring(0, 7)} ${doc.title}`
+  `* ${chalk.yellow(title(doc))} ${doc.title}`
 
 // Single formatting function
 const formatter = OPTS.format.toLowerCase() == 'json' ?
